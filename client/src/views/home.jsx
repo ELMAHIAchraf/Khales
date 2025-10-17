a768e7c2941a7d6fd02869178ca76780bcf2d65b
e.achraf <elmahi.achraf9@gmail.com>
2025-10-17 08:16:52 +0200

    Init: configured both the client and server apps

---
+++ client/src/views/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Greet } from '../components/greet.jsx'
+
+export const Home = () => {
+  return (
+    <div>
+        <b>Home Page</b>
+        <Greet/>
+    </div>
+  )
+}
